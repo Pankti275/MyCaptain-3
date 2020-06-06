c03d7ddbcf2870ae0ba49f74af223ef82ad73827
Pankti275 <66326473+Pankti275@users.noreply.github.com>
2020-06-06 16:31:59 +0530

    Create assignment 3.c

---
+++ assignment 3.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+int main()
+{
+
+int marks=0;
+scanf("%d",&marks);
+if (marks<40)
+{
+printf("Grade F");
+}
+
+{
+
+}
+
+{
+printf("Grade C");
+}
+else if (marks>=70 && marks<=84)
+{
+printf("Grade B");
+}
+
+
+
+
