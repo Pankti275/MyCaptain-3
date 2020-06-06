79734c1f289eacd2fd3304634f855ee46ab95b11
Pankti275 <66326473+Pankti275@users.noreply.github.com>
2020-06-06 16:56:03 +0530

    Created assignment 3
    What do I eat?

--- assignment 3.c
+++ assignment 3.c
@@ -1,26 +1,55 @@
+#include <stdio.h>
 
-#include<stdio.h>
 int main()
 {
-
-int marks=0;
-scanf("%d",&marks);
-if (marks<40)
+ printf("What to order?: \n1. Sandwich\n2. Burger\n3. Pizza\n4. Paasta\n5. French Fries");
+int choice=0;
+scanf("%d", &choice);
+switch(choice)
 {
-printf("Grade F");
+  case 1:
+    printf("Food item - Sandwich\n Price - Rs.149");
+    break;
+  case 2:
+    printf("Food item - Burger\n Price - Rs.129");
+    break;
+  case 3:
+    printf("Food item - Pizza\n Price - Rs.239");
+    break;
+  case 4:
+    printf("Food item - Paasta\n Price - Rs.179");
+    break;
+  case 5:
+    printf("Food item - French Fries\n Price - Rs.99");
+    break;
+  default: printf("Use the default case");
 }
 
-{
-
 }
 
-{
-printf("Grade C");
-}
-else if (marks>=70 && marks<=84)
-{
-printf("Grade B");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
